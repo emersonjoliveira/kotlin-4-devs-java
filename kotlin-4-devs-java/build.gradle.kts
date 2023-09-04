82f1f5635a4e11157be86d2e48528276cad8a8f5
emerson.oliveira <emersonjdeoliveira@gmail.com>
2023-09-04 08:17:12 -0300

    Initial configurations

---
+++ kotlin-4-devs-java/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "br.com.emerson"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
